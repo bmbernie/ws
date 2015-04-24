ffe96aff74674a3d3ecc14c32ea4d04305bd2a5a
Brandon Bernie <bmbernie@gmail.com>
2015-04-24 15:55:15 -0800

    Added support for header security middleware

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/bmbernie/httprouter"
+	"github.com/bmbernie/ws/middleware"
+	"github.com/bmbernie/ws/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Setup Routes
+	router := httprouter.New()
+	router.GET("/", routes.Index)
+
+	// Setup Middleware
+	mh := make(middleware.MiddlewareHandler)
+	mh["bpf.io"] = router
+
+	log.Fatal(http.ListenAndServe(":80", mh))
+}
---
+++ middleware/middleware.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+type MiddlewareHandler map[string]http.Handler
+
+const (
+	// stsHeader = "Strict-Transport-Security: max-age=31536000; includeSubDomains"
+	frameOptionsHeader  = "X-Frame-Optionsi: Deny"
+	contentTypeHeader   = "X-Content-Type-Options: nosniff"
+	xssProtectionHeader = "X-XSS-Protection: 1; mode=block"
+	cspHeader           = "Content-Security-Policy: default-src 'self'"
+)
+
+func (mh MiddlewareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Basic Logging Middleware
+	log.Println(r.RemoteAddr, ":", r.Method, r.RequestURI, r.Proto)
+
+	// Security Headers
+	AddSecurityHeaders(w)
+
+	if mh[r.Host] == nil {
+		http.Error(w, "Forbidden", 403)
+		log.Println("+-->  HostHeaderSecurityMiddleware -- Invalid Host", r.Host)
+		return
+	}
+
+	mh[r.Host].ServeHTTP(w, r)
+}
+
+// TODO: Enable HSTS when TLS is implemented
+//
+func AddSecurityHeaders(w http.ResponseWriter) {
+	headers := make(map[string]string)
+	// headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+	headers["X-Content-Type-Options"] = "nosniff"
+	headers["X-XSS-Protection"] = "1; mode=block"
+	headers["Content-Security-Policy"] = "default-src 'self'"
+
+	for k, v := range headers {
+		w.Header().Add(k, v)
+	}
+}
