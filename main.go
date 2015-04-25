2b689bfc349f1def964225d947e3ef2e8059d02f
Brandon Bernie <bmbernie@gmail.com>
2015-04-24 16:22:55 -0800

    adding back in empty routes folder

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Setup Routes
 	router := httprouter.New()
-	router.GET("/", routes.Index)
+	router.GET("/", routes.indexHandler)
 
 	// Setup Middleware
 	mh := make(middleware.MiddlewareHandler)
